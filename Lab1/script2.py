8f9016cb9f996bf9a9f1dd314f47b129a0109675
ishan <hi@ishaanx.com>
2021-09-08 14:04:00 +0530

    update progress

--- Lab1/script2.py
+++ Lab1/script2.py
@@ -86,16 +86,84 @@ def compare_lists(list1,list2):
 print(compare_lists([1,2,3,4],[5,6,7,0]))
 
 #12 Write a Python function that takes two lists and returns True if they have at least one common member. 
-
 list1 = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 print ('Original list is :',list1)
-
 del list1[5]
 del list1[4]
 del list1[0]
 print('Modified list is: ',list1)
 
 #13 3d array
-
 symbol = [[ ['*' for col in range(3)] for col in range(4)] for row in range(6)]
 print(symbol)
+
+#14 remove even numbers from list
+list = [1,2,3,4,5]
+print("Original List")
+print (list)
+for x in list:
+    if(x%2==0):
+        list.remove(x)
+print("New list is: ",list)
+
+#15 Shuffle a list
+import random
+list = [1,2,3,4,5]
+print("Original list is: ",list)
+random.shuffle(list)
+print("Shuffled list is: ",list)
+
+#16 
+
+def printValues():
+	l = []
+	for i in range(1,21):
+		l.append(i**2)
+	print(l[:5])
+	print(l[-5:])
+
+printValues()
+
+#17 
+def printValues2():
+	l = []
+	for i in range(1,21):
+		l.append(i**2)
+	print(l[5:])
+	print(l[:-5])
+
+printValues2()
+
+# 18 all permutations
+from itertools import permutations 
+  
+  
+a = "Ishan"
+  
+# no length entered so default length
+# taken as 4(the length of string GeEK)
+p = permutations(a) 
+  
+# Print the obtained permutations 
+for j in list(p): 
+    print(j)
+
+#19 difference between two lists
+list_1 = [5, 10, 15, 20, 25, 30]
+list_2 = [10, 20, 30, 40, 50, 60]
+
+difference_1 = set(list_1).difference(set(list_2))
+difference_2 = set(list_2).difference(set(list_1))
+
+list_difference = list(difference_1.union(difference_2))
+print(list_difference)
+
+
+#20 Access index of a list
+mylist = [21, 5, 8, 52, 21, 87]
+item = 8
+
+#search for the item
+index = mylist.index(item)
+
+print('The index of', item, 'in the list is:', index)
